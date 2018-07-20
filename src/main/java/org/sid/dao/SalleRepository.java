153ed63c82573dc112454946f36373a8d2d9183d
BELKATI samir <s_belkati@esi.dz>
2018-07-21 01:42:38 +0200

    Service Web en utilisant DATA REST et des méthodes spécifique

--- src/main/java/org/sid/dao/SalleRepository.java
+++ src/main/java/org/sid/dao/SalleRepository.java
@@ -1,10 +1,19 @@
 package org.sid.dao;
 
 import org.sid.entities.Salle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface SalleRepository extends JpaRepository<Salle, Long>{
-
+ 
+	public Page<Salle> findByNomContains(@Param("mc") String mc, Pageable pageable);
+	
+	
+	@RestResource(path="/parNom")
+	public Page<Salle> findByNomStartsWith(@Param("mc") String mc, Pageable pageable);
 }
